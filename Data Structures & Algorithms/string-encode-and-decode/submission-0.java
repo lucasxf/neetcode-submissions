e82f4e320c40c20c90a6f0b401b81df4e80bc9f6
lucasxf <lucasxferreira@gmail.com>
2026-04-05 13:30:42 -0300

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.java
@@ -0,0 +1,40 @@
+class Solution {
+
+    public String encode(List<String> strs) {
+        // PROBLEM: codificar uma lista/vetor de strings numa string única 
+        // e depois ser capaz de converter de volta para uma lista de stirngs
+        // INPUT: lista de strings // OUTPUT: string única codificada (ou vice-versa)
+        // EXAMPLE: "["Bom", " dia", ",", " Lucas!"]" -> "Bom dia, Lucas!" (e vice-versa)
+        // PATTERN: não sei
+        // APPROACH: eu pensei em utilizar prefixos ou sufixos na string, mas para suportar quaisquer caracteres isso pode ser um problema
+        // vou tentar utilizar regex
+        // COMPLEXITY: nem desconfio
+        String encodedString = "";
+        String prefix = "#";
+        for (String s : strs) {
+            int size = s.length();
+            encodedString += size + prefix + s;
+        }
+        return encodedString;
+    }
+
+    public List<String> decode(String str) {
+        List<String> decodedStrings = new ArrayList<>();
+        int i = 0;
+        int j = str.indexOf('#', i);
+        System.out.println("str: [" + str + "] | i: " + i + " | j:" + j);
+        while (i < str.length()) {
+            // number str before # prefix
+            String lengthStr = str.substring(i, j);
+            System.out.println("lengthStr: " + lengthStr);
+            // parse to length
+            int nextWordLength = Integer.parseInt(lengthStr);
+            // store found string
+            decodedStrings.add(str.substring(j+1, nextWordLength + j + lengthStr.length()));
+            System.out.println("decoded: " + decodedStrings);
+            i += nextWordLength + lengthStr.length() + 1;
+            j = str.indexOf('#', i);
+        }
+        return decodedStrings;
+    }
+}
